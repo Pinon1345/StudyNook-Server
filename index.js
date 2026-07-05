ea5ced312709c8e189aae6d2a014ce2328a0b9e5
Pinon1345 <pinonfurkan1@gmail.com>
2026-07-05 18:11:25 +0600

    Implement POST API for creating new room and collecting json data to keep in db.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config()
@@ -9,7 +10,10 @@ const uri = process.env.MONGODB_URI;
 const app = express()
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -19,16 +23,37 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function run() {
+
     try {
+
         // Connect the client to the server	(optional starting in v4.7)
+
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+        const db = client.db("studynook")
+        const roomCollection = db.collection("rooms")
+
+
+        // Post API for Adding New Room
+
+        app.post('/room', async (req, res) => {
+            const roomData = req.body
+            console.log(roomData)
+            const result = await roomCollection.insertOne(roomData)
+
+            res.json(result)
+
+        })
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
